dbf940a17d626e0c9423e341583be95262c68cbf
taco <nnnantai-doubutsu@yahoo.co.jp>
2024-01-01 03:14:51 +0900

    シンボルselect:をselect_all:に変更

--- memo.rb
+++ memo.rb
@@ -10,7 +10,7 @@ require 'sinatra/reloader'
 DB_NAME = 'memoapp'
 QUERIES = {
   select_one: 'SELECT * FROM memo WHERE id = $1',
-  select: 'SELECT * FROM memo ORDER BY created_at',
+  select_all: 'SELECT * FROM memo ORDER BY created_at',
   insert: 'INSERT INTO memo (id, title, text) VALUES ($1, $2, $3)',
   update: 'UPDATE memo SET title = $1, text = $2 WHERE id = $3',
   delete: 'DELETE FROM memo WHERE id = $1'
@@ -27,7 +27,7 @@ def manipulate_db(manipulation, memo = nil)
   query = QUERIES[manipulation]
   case manipulation
   when :select_one then conn.exec_params(query, [memo[:id]]).first
-  when :select then conn.exec_params(query)
+  when :select_all then conn.exec_params(query)
   when :insert then conn.exec_params(query, [memo[:id], memo[:title], memo[:text]])
   when :update then conn.exec_params(query, [memo[:title], memo[:text], memo[:id]])
   when :delete then conn.exec_params(query, [memo[:id]])
@@ -36,7 +36,7 @@ end
 
 get '/' do
   @title = 'メモ一覧'
-  @memos = manipulate_db(:select)
+  @memos = manipulate_db(:select_all)
   erb :index
 end
 
